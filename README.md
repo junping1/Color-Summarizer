ab66c103c7007b6c4ae871449612b28e3a7e64d6
Junping <jasoikwanyi@gmail.com>
2021-03-19 04:24:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,134 @@
+# Color Summarizer
+
+[demo](https://jpcolor.netlify.app/)
+
+Color Summarizer can analyze thousands of color palettes of images and generate a 3D color model corresponding to these color palettes.
+
+Even if you are not familiar with programming, you can use Color Summarizer with some simple configurations.
+
+Color Summarizer supports images in `jpg`, `png`, `jpeg` formats.
+
+## How to use in macOS
+
+### 1. [Download](https://github.com/Itsukay/Color-Summarizer/archive/master.zip) Color Summarizer
+
+After downloading, double click to unzip, you should see a folder named `Color-Summarizer-master`.
+
+### 2. Download and install [Anaconda Installer](https://www.anaconda.com/products/individual)
+
+There are two download options, using Graphical Installer makes the installation easier.
+
+### 3. Open Terminal in the folder where Color Summarizer is located
+
+Right-click on the folder (`Color-Summarizer-master`) and select **_Services > New Terminal at Folder_** to open Terminal.
+
+### 4. Install `colorgram.py`
+
+Copy the following command, paste it into `Terminal`, and press ENTER.
+
+```bash
+pip3 install colorgram.py
+```
+
+You should see something is successfully installed.
+
+### 5. Now try to run Color Summarizer!
+
+Copy the following command, paste it into `Terminal`, and press ENTER.
+
+```bash
+python3 run.py
+```
+
+You should see the following output
+
+```bash
+Please enter the name of the folder you want to analyse:
+```
+
+Type `img` and press ENTER.
+
+You should see a message that Color Summarizer ran successfully, and a webpage presents the results.
+
+### 6. Now try with your images!
+
+Create a new folder (e.g., `my-images`) in the folder where Color Summarizer is located and copy the your images into that folder. Then copy the following command and press the ENTER keyboard.
+
+```bash
+python3 run.py
+```
+
+Enter the folder name (in this case `my-images`) and press ENTER. Color Summarizer will tell you the estimated time of completion and will automatically display the results when it is done.
+
+Results are store with name `[img-folder-name]_result.html`.
+
+That's it!
+
+****
+<br>
+
+# Color Summarizer（中文说明）
+
+[演示](https://jpcolor.netlify.app/)
+
+Color Summarizer 可以分析成千上万张图像的调色板，并生成与这些调色板对应的 3D 色彩模型。
+
+即使你不熟悉编程，你也可以通过一些简单的配置来使用 Color Summarizer。
+
+Color Summarizer 支持`jpg`、`png`、`jpeg`格式的图像。
+
+## 如何在 macOS 中使用
+
+### 1. [下载](https://github.com/Itsukay/Color-Summarizer/archive/master.zip)Color Summarizer
+
+下载后，双击解压，你应该会看到一个名为 "Color-Summarizer-master "的文件夹。
+
+### 2. 下载并安装 [Anaconda Installer](https://www.anaconda.com/products/individual)
+
+有两种下载方式，使用 Graphical Installer 安装更容易。
+
+### 3. 在 Color Summarizer 所在的文件夹中打开终端。
+
+右击文件夹(`Color-Summarizer-master`)，选择**_Services > New Terminal at Folder_**打开终端。
+
+### 4. 安装`colorgram.py`。
+
+复制下面的命令，粘贴到 "终端 "中，并按回车键。
+
+```bash
+pip3 install colorgram.py
+```
+
+你应该看到一些东西被成功安装。
+
+### 5. 现在尝试运行 Color Summarizer!
+
+复制下面的命令，粘贴到 "终端"，然后按 ENTER 键。
+
+```bash
+python3 run.py
+```
+
+你应该看到以下输出
+
+```bash
+Please enter the name of the folder you want to analyse:
+```
+
+输入`img`，然后按 ENTER 键。
+
+你应该会看到一条消息，说 Color Summarizer 运行成功，并在网页上显示结果。
+
+### 6. 现在试试你的图像
+
+在 Color Summarizer 所在的文件夹中创建一个新的文件夹(例如，`my-images`)，并将你的图片复制到该文件夹中。然后复制下面的命令，按 ENTER 键。
+
+```bash
+python3 run.py
+```
+
+输入文件夹名称（本例中为 "my-images"），按ENTER键。Color Summarizer会告诉你完成的预计时间，并在完成后自动显示结果。
+
+分析结果会以`[img-文件夹名]_result.html`的名称存储。
+
+就这样!
---
+++ run.py
@@ -0,0 +1,190 @@
+import requests
+import re
+import os
+from pathlib import Path
+import colorgram
+import numpy as np
+import pandas as pd
+from multiprocessing import Pool
+import multiprocessing
+import webbrowser
+from datetime import datetime, timedelta
+
+img_color = []
+pool_num = multiprocessing.cpu_count()
+
+
+def get_folder_img_path(folder_path):
+    paths = sorted(Path(folder_path).iterdir(), key=os.path.getmtime)
+    path_list = ['/'.join(x.parts) for x in paths]
+    filtered_path_list = list(filter(
+        lambda name: '.png' in name or '.jpeg' in name or '.jpg' in name, path_list))
+    return filtered_path_list
+
+
+def extract_color(file_path):
+    colors = colorgram.extract(file_path, 5)
+    img_data = {}
+    img_data["file"] = file_path
+    img_data["color"] = []
+    for color in colors:
+        color_data = {}
+        color_data["RGB"] = tuple([color_value for color_value in color.rgb])
+        color_data['HEX'] = '#%02x%02x%02x' % tuple(
+            [color_value for color_value in color.rgb])
+        color_data["HSL"] = tuple([color_value for color_value in color.hsl])
+        color_data["percentage"] = str(round(color.proportion * 100, 2)) + "%"
+        img_data["color"].append(color_data)
+    return img_data
+
+
+def generate_result(img_color):
+    style_html = '''<style>
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .img-info {
+        padding: 10px;
+    }
+
+    img {
+        height: 200px;
+        padding-bottom: 10px;
+    }
+
+    table {
+        display: none;
+    }
+
+    th,
+    td {
+        text-align: center;
+        /* padding-left: 10px; */
+        padding-right: 20px;
+        text-align: center;
+        padding: 5px;
+    }
+
+    .img-result {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .color {
+        height: 20px;
+        width: 20px;
+        border-radius: 50%;
+    }
+
+    .color-palette {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        height: 20px;
+    }
+
+    .container {
+        max-width: 1400px;
+        margin: auto;
+        display: grid;
+        grid-gap: 4rem;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    }
+    </style>'''
+
+    result_list = []
+    for img in img_color:
+        color_palette = ''.join(['<div class="color-palette_color" style="background-color: ' + str(
+            x['HEX'])+'; width: ' + str(x['percentage'])+';"></div>' for x in img['color']])
+        table_info = ''.join(['<tr><td><div class="color" style="background-color: ' + str(x['HEX'])+';"></div></td><td>' + str(x['RGB']) +
+                              '</td><td>' + str(x['HEX'])+'</td><td>' + str(x['HSL'])+'</td><td>' + str(x['percentage'])+'</td></tr>' for x in img['color']])
+        img_info = '<div class="img-info"><img src="{}"></img><div class="color-palette">{}</div></div>'.format(
+            img['file'], color_palette)
+        table = '<table><tr><th>COLOR</th><th>RGB</th><th>HEX</th><th>HSL</th><th>PERCENT</th></tr>{}</table>'.format(
+            table_info)
+        img_result = '<div class="img-result">{}{}</div>'.format(
+            img_info, table)
+        result_list.append(img_result)
+
+    output_html = style_html + '<div class="container">' + \
+        str(''.join(result_list)) + '</div>'
+
+    with open(str(input_path)[:-1] + "_result.html", "w") as output:
+        output.write(output_html)
+
+    r, g, b = [[], [], []]
+    for each in [x['color'] for x in img_color]:
+        r += ([x['RGB'][0] for x in each])
+        g += ([x['RGB'][1] for x in each])
+        b += ([x['RGB'][2] for x in each])
+
+    rdata = np.array(r)
+    gdata = np.array(g)
+    bdata = np.array(b)
+
+    dot_color = []
+    for b, r, g in zip(bdata, rdata, gdata):
+        dot_color += ["rgb({},{},{})".format(r, g, b)]
+
+    color_df = pd.DataFrame({"b": bdata, "r": rdata, "g": gdata})
+
+    import plotly.express as px
+    fig = px.scatter_3d(color_df, x='b', y='r', z='g',
+                        title="Summary of color palettes",
+                        width=1000, height=1000
+                        )
+
+    fig.update_layout(
+        scene=dict(
+            xaxis_showspikes=False,
+            yaxis_showspikes=False,
+            zaxis_showspikes=False,
+            xaxis=dict(nticks=2, range=[255, 0],
+                       backgroundcolor='rgba(0,0,0,0)'),
+            yaxis=dict(nticks=2, range=[0, 255],
+                       backgroundcolor='rgba(0,0,0,0)'),
+            zaxis=dict(nticks=2, range=[0, 255],
+                       backgroundcolor='rgba(0,0,0,0)'),
+            xaxis_title='BLUE',
+            yaxis_title='RED',
+            zaxis_title='GREEN',
+        ),
+    )
+
+    fig.update_traces(marker=dict(color=dot_color))
+
+    fig.write_html("color-3d.html")
+
+    with open("color-3d.html", "r") as color_3d:
+        color_3d_html = color_3d.read()
+
+    print(str(input_path)[:-1] + "_result.html")
+
+    with open(str(input_path)[:-1] + "_result.html", "w") as output:
+        output.write(color_3d_html + output_html)
+
+
+if __name__ == '__main__':
+    input_path = input(
+        "Please enter the name of the folder you want to analyse: ") + '/'
+
+    path_list = get_folder_img_path(input_path)[:]
+    print("analysing {} pics...".format(len(path_list)))
+
+    est_secs = 0.4 * 8 / pool_num * len(path_list)
+    est_end_time = datetime.now() + timedelta(seconds=est_secs)
+    print("est. completed time: {}, {} mins later!".format(
+        est_end_time.strftime("%H:%M:%S"), round(est_secs/60, 2)))
+
+    with Pool(pool_num) as p:
+        img_color += p.map(extract_color, path_list)
+        generate_result(img_color)
+        print("✅ completed")
+        result_path = str('file://' + os.getcwd()) + '/' + \
+            str(input_path)[:-1] + "_result.html"
+        print("check result at {}".format(result_path))
+        webbrowser.open(result_path, new=2)
